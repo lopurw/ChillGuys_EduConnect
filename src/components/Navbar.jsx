d5a3e988393a60be4f2bd7be49772242f2fd220e
Lipopoyo <krasev100@mail.ru>
2024-12-10 17:00:00 +0300

    General.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import '../styles/Navbar.module.css';
+import classes from '../styles/Navbar.module.css';
 
 const Navbar = () => {
-  const [role, setRole] = useState('');
-
-  
+  const [role, setRole] = useState('Студент');
 
   return (
-    <nav>
-      <ul>
-        
-        {role === 'Студент' ? (
-          <li>
-            <NavLink
-              to="/homeuser"
-              className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
-            >
-               EduConnect1
-            </NavLink>
-          </li>
-        ) : (
-          <li>
-            <NavLink
-              to="/home"
-              className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
-            >
-              Home
-            </NavLink>
-          </li>
-        )}
-        <li>
-          <NavLink
-            to="/login"
-            className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
-          >
-            Вход
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/register"
-            className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
-          >
-            Регистрация
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+      <div className={classes.wrapper}>
+        <div className={classes.container}>
+          <div className={classes.nav_wrapper}>
+              {role === 'Студент' ? (
+                  <div>
+                    <NavLink
+                        to="/homeuser"
+                        className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+                    >
+                      EduConnect
+                    </NavLink>
+                  </div>
+              ) : (
+                  <div>
+                    <NavLink
+                        to="/home"
+                        className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+                    >
+                      Главная
+                    </NavLink>
+                  </div>
+              )}
+            <div className={classes.side_buttons}>
+              <div className={classes.side_button}>
+                <NavLink
+                    to="/login"
+                    className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+                >
+                  Вход
+                </NavLink>
+              </div>
+              <div className={classes.side_button}>
+                <NavLink
+                    to="/register"
+                    className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+                >
+                  Регистрация
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </nav>
+
   );
 };
 
