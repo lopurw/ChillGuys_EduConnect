305b3a414c0f7848f101d129ac48bdab7097a2d8
gleb-zhurav1ev <glebzuravlev2005@gmail.com>
2024-12-10 16:29:06 +0300

    minimal changes HomeUser.jsx

--- src/pages/HomeUser.jsx
+++ src/pages/HomeUser.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import { register } from '../services/ApiServ'; 
 import StatisticsChart from '../components/StatisticsUserHome';
+import classes from '../styles/HomeUser.module.css';
 const HomeUser = () => {
   
 
   return (
-    <>
+    <div className={classes.main}>
     <h1>Начинай обучение уже сейчас!</h1>
-    <button>Поиск курсов</button>
-    <button>Вакансии</button>
-    <button>Проекты</button>
-    <button>Мои курсы</button>
+    <div className={classes.buttons_container}>
+      <button>Поиск курсов</button>
+      <button>Вакансии</button>
+      <button>Проекты</button>
+      <button>Мои курсы</button>
+    </div>
     <StatisticsChart></StatisticsChart>
-    </>
+    </div>
   );
 };
 
