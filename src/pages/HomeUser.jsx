1339262eecb2049ddc66c10bb17333b4b48f18e5
gleb-zhurav1ev <glebzuravlev2005@gmail.com>
2024-12-10 18:51:05 +0300

    ProjectCard + ProjectDetail + ProjectList + Comment

--- src/pages/HomeUser.jsx
+++ src/pages/HomeUser.jsx
@@ -7,40 +7,37 @@ import classes from '../styles/HomeUser.module.css';
 import { NavLink } from 'react-router-dom';
 
 const HomeUser = () => {
-  
-
-  return (
-    <>
-    <div className={classes.main}>
-    <h1>Начинай обучение уже сейчас!</h1>
-    <div className={classes.buttons_container}>
-    <NavLink
-          to="/coursesuser" 
-          className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
-        >
-          <button className={classes.button}>{' '}
-					<img src="/public/search.png" alt="" /> Поиск курсов</button>
-        </NavLink>
-   <button className={classes.button}>
-					{' '}
-					<img src="/public/vacancy.png" alt="" />
-					Вакансии
-				</button>
-				<button className={classes.button}>
-					{' '}
-					<img src="/public/project.png" alt="" /> Проекты
-				</button>
-				<button className={classes.button}>
-					{' '}
-					<img src="/public/courses.png" alt="" /> Мои курсы
-				</button>
-    <StatisticsChart></StatisticsChart>
-    </div>
-</div>
-
-    </>
-  );
-
+	return (
+		<>
+			<div className={classes.main}>
+				<h1>Начинай обучение уже сейчас!</h1>
+				<div className={classes.buttons_container}>
+					<NavLink to="/coursesuser" className={({ isActive }) => (isActive ? 'activeLink' : 'link')}>
+						<button className={classes.button}>
+							{' '}
+							<img src="/public/search.png" alt="" /> Поиск курсов
+						</button>
+					</NavLink>
+					<button className={classes.button}>
+						{' '}
+						<img src="/public/vacancy.png" alt="" />
+						Вакансии
+					</button>
+          <NavLink to="/projectuser" className={({ isActive }) => (isActive ? 'activeLink' : 'link')}>
+					<button className={classes.button}>
+						{' '}
+						<img src="/public/project.png" alt="" /> Проекты
+					</button>
+          </NavLink>
+					<button className={classes.button}>
+						{' '}
+						<img src="/public/courses.png" alt="" /> Мои курсы
+					</button>
+				</div>
+				<StatisticsChart></StatisticsChart>
+			</div>
+		</>
+	);
 };
 
 export default HomeUser;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
@@ -7,21 +6,25 @@ import HomeUser from '../pages/HomeUser';
 import Home from '../pages/Home';
 import CourseList from '../components/CourseList';
 import CourseDetail from '../components/CourseDetail';
+import ProjectList from '../components/ProjectList';
+import ProjectDetail from '../components/ProjectDetail';
+import Comment from '../components/Comment';
 
 const AppRoutes = () => {
-  return (
-    
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/homeuser" element={<HomeUser />} />
-        <Route path="/coursesuser" element={<CourseList />} />
-        <Route path="/course/:id" element={<CourseDetail />} />
-      </Routes>
-   
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Dashboard />} />
+			<Route path="/home" element={<Home />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/register" element={<Register />} />
+			<Route path="/homeuser" element={<HomeUser />} />
+			<Route path="/coursesuser" element={<CourseList />} />
+			<Route path="/course/:id" element={<CourseDetail />} />
+			<Route path="/projectuser" element={<ProjectList />} />
+			<Route path="//project/:id" element={<ProjectDetail />} />
+			<Route path="comment" element={<Comment />} />
+		</Routes>
+	);
 };
 
 export default AppRoutes;
