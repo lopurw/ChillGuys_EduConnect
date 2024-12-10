f311bbf26ed83a133bb131d09ed588f7b9e04b09
gleb-zhurav1ev <glebzuravlev2005@gmail.com>
2024-12-10 14:19:58 +0300

    стили для логина

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,19 @@
-
+import classes from '../styles/Login.module.css';
 
 const Login = () => {
-  return (
-    <div>
-      <h1>Login Page</h1>
-      {/* Форма авторизации */}
-      <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			{/* <h1>Login Page</h1> */}
+			{/* Форма авторизации */}
+			<form>
+				<input type="email" placeholder="Почта" />
+				<input type="password" placeholder="Пароль" />
+				<button className={classes.button} type="submit">
+					Login
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Login;
