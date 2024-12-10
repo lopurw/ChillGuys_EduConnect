1c862c3c312dca953acfdfdd6ba1c6d4aec585bd
gleb-zhurav1ev <glebzuravlev2005@gmail.com>
2024-12-10 16:36:56 +0300

    HomeUser.jsx

--- src/pages/HomeUser.jsx
+++ src/pages/HomeUser.jsx
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
-import { register } from '../services/ApiServ'; 
+import { register } from '../services/ApiServ';
 import StatisticsChart from '../components/StatisticsUserHome';
 import classes from '../styles/HomeUser.module.css';
 const HomeUser = () => {
-  
-
-  return (
-    <div className={classes.main}>
-    <h1>Начинай обучение уже сейчас!</h1>
-    <div className={classes.buttons_container}>
-      <button>Поиск курсов</button>
-      <button>Вакансии</button>
-      <button>Проекты</button>
-      <button>Мои курсы</button>
-    </div>
-    <StatisticsChart></StatisticsChart>
-    </div>
-  );
+	return (
+		<div className={classes.main}>
+			<h1>Начинай обучение уже сейчас!</h1>
+			<div className={classes.buttons_container}>
+				<button className={classes.button}>Поиск курсов</button>
+				<button className={classes.button}>Вакансии</button>
+				<button className={classes.button}>Проекты</button>
+				<button className={classes.button}>Мои курсы</button>
+			</div>
+			<StatisticsChart></StatisticsChart>
+		</div>
+	);
 };
 
 export default HomeUser;
