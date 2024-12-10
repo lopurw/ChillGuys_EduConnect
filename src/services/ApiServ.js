28ea37f2d1d76ee83b6694a9f58221a447c07f7a
lopurw <semashkoarina2006@gmail.com>
2024-12-10 13:35:49 +0300

    регистрация

---
+++ src/services/ApiServ.js
@@ -0,0 +1,12 @@
+
+import axios from 'axios';
+const apiClient = axios.create({    baseURL: 'http://localhost:2211/api', 
+    headers: {        'Content-Type': 'application/json',
+    },});
+
+
+
+    export const register = async (userData) => {    const response = await apiClient.post('/Auth/Register', userData);
+        return response.data;};
+
+
