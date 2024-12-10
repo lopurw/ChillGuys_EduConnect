cbd3b3302fab69f2a8a807aba31b12433a0b78d5
lopurw <semashkoarina2006@gmail.com>
2024-12-10 15:09:13 +0300

    роль

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,12 +4,14 @@ import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Dashboard from '../pages/Home';
 import HomeUser from '../pages/HomeUser';
+import Home from '../pages/Home';
 
 const AppRoutes = () => {
   return (
     
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/homeuser" element={<HomeUser />} />
